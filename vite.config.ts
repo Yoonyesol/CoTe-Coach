1c4265df6176179cbc4919613bef07fb25663c7b
yoonyesol <maniac970430@naver.com>
2026-01-02 23:20:35 +0900

    Config: 배포 에러에 따른 배포 설정 변경
    - Failed to load module script: Expected a JavaScript-or-Wasm module script but the server responded with a MIME type of "application/octet-stream". Strict MIME type checking is enforced for module scripts per HTML spec. 에러 해결

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,27 @@ import tailwindcss from '@tailwindcss/vite'
 import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/CoTe-Coach/',
-  plugins: [react(), tailwindcss()],
-  css: {
-    postcss: {
-      plugins: [autoprefixer()],
-    },
-  },
-  server: {
-    proxy: {
-      '/api/v3': {
-        target: 'https://solved.ac',
-        changeOrigin: true,
+export default defineConfig(({ command }) => {
+  return {
+    // 깃허브 배포 시에는 레포지토리 이름을, 로컬 개발 시에는 루트(/)를 사용
+    base: command === 'build' ? '/CoTe-Coach/' : '/',
+    plugins: [react(), tailwindcss()],
+    css: {
+      postcss: {
+        plugins: [autoprefixer()],
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api/v3': {
+          target: 'https://solved.ac',
+          changeOrigin: true,
+        },
+      },
+    },
+    // 빌드 결과물이 dist 폴더로 나가도록 명시 (gh-pages 연동)
+    build: {
+      outDir: 'dist',
+    }
+  }
 })
